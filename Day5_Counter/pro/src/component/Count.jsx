04a64ea8af991dfa1ff7afc0c93e233934c6d322
Alpeshsorte <alpeshsorte4@gmail.com>
2026-06-04 22:34:49 +0530

    change text

--- Day5_Counter/pro/src/component/Count.jsx
+++ Day5_Counter/pro/src/component/Count.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+// what is hooks 
+// Hooks are special functions that let you use React features like state, lifecycle methods, and context in functional components.
+
+// useState is a React Hook that allows a component to store and update data (state). When the state changes, React automatically updates the UI.
+
+import React, { use } from 'react'
 
 import { useState } from 'react' 
 
@@ -15,54 +20,73 @@ export default function Count() {
 
     let [font, setfont]=useState(30)
 
-    let arr_font = ['Times New Roman', 'Arial', 'Georgia', 'Courier New', 'Verdana', 'Impact'];
+    let arr_font = ['Times New Roman', 'Arial'];
+
+    let [text, settext]=useState("Alpesh")
+
+    let [btn, setbtn]=useState(false)
 
     function inc(){
         change(count+1)
-        // point_change(count)
-        // point_change((count+1) % color.length)
-        // if(point >= color.length-1){
-        //     print_change(0)
-        // }else{
-        //     point_change(point+1)
+        point_change(count)
+        point_change((count+1) % color.length)
+        if(point >= color.length-1){
+            print_change(0)
+        }else{
+            point_change(point+1)
+        }
+
+        chnage_font(font)
+
+        chnage_font(+2)
+
+        // if(count%2==0){
+        //     setfont(arr_font())
         // }
 
-        // chnage_font(font)
-
-        if(count%2==0){
-
-            setfont(arr_font())
-
+        if(count>=4){
+            alert("react developer ")
+            setbtn(true)
         }
+
     }
 
     function dec(){
         change(count-1)
-        // point_change(count+1)
-        // point_change((count+1) % color.length)
-        // if(point<=0){
-        //     point_change(color.length-1)
-        // }else{
-        //     point_change(point-1)
+        point_change(count+1)
+        point_change((count+1) % color.length)
+        if(point<=0){
+            point_change(color.length-1)
+        }else{
+            point_change(point-1)
+        }
+
+        chnage_font(-font)
+
+        chnage_font(-2)
+
+        // if(count%2!=0){
+        //     setfont(font-(count*2))
         // }
 
-        // chnage_font(-font)
-
-        if(count%2!=0){
-            setfont(font-(count*2))
-        }
+    
     }
 
     function chnage_font(val){
         setfont(font+(val*2))
     }
 
+    function text_change(res){
+        settext()
+
+    }
 
   return (
     <div style={{width:'200px',display:'flex',margin:'auto', gap:'10px'}}>
         <button onClick={dec}>decriment</button>
-        <h1 style={{color:color[point], fontSize:arr_font[font]}}>{count}</h1>
-        <button onClick={inc}>incriment</button>
+        <h1 style={{color:color[point], fontSize:font, fontFamily: arr_font[font % 2 ===0] ?  "Arial" : "Courier New" }}>{count}</h1>
+        <h1 style={{color:color[point], fontSize:font, fontFamily: arr_font[font % 2 ===0] ?  "Arial" : "Courier New" }}>{text}</h1>
+        <button disabled={btn} onClick={inc}>incriment</button>
     </div>
   )
 }
