9bc67238dfc4b20fc980d998bcba65ba4186a8bb
Alpeshsorte <alpeshsorte4@gmail.com>
2026-06-11 23:58:52 +0530

    chanke

--- practice/course/src/App.jsx
+++ practice/course/src/App.jsx
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function App() {
+ let [data, setdata] = useState([
+    {
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcR5U16C8yXgBpl7-Bc7Itjx3_LRl425zINA&s",
+      Course_name: "react",
+      Trainer_name: "rushikesh sir",
+      Enrolled_main: "Enrolled"
+    },
+    {
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYp6eevBPO-iP745QNbvacS-yjQ1tOtmwlkg&s",
+      Course_name: "python",
+      Trainer_name: "MK sir",
+      Enrolled_main: "Enrolled"
+    },
+    {
+      image: "https://logos-world.net/wp-content/uploads/2025/07/Django-Logo.png",
+      Course_name: "Django",
+      Trainer_name: "Alpesh sir",
+      Enrolled_main: "Enrolled"
+    },
+    {
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlGdAXqFAzC873pjFmVEOqdpot0I2nRqwxMA&s",
+      Course_name: "javascript",
+      Trainer_name: "Sanket sir",
+      Enrolled_main: "UnEnrolled"
+    }
+  ])
   return (
     <div>App</div>
   )
