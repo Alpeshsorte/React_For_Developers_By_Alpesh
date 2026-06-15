a328fa6559aa07d77b86402b85fbd0a157b43d7a
Alpeshsorte <alpeshsorte4@gmail.com>
2026-06-15 21:06:31 +0530

    small change

--- Employee_cart/Employee/src/App.jsx
+++ Employee_cart/Employee/src/App.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react'
-// import Item from './componenets/Item'
-import Data from './componenets/Data'
-import Cart from './componenets/Cart'
+import Item from './componenets/Item'
+// import Data from './componenets/Data'
+// import Cart from './componenets/Cart'
 export default function App() {
 
   return (
     <div style={{display:"grid", gridTemplateColumns:'repeat(3,1fr)'}}>
        
-        {/* // <Item/> */}
-        <Data/>
-        <Cart/>
+        // <Item/>
+        {/* <Data/>
+        <Cart/> */}
     </div>
   )
 }
--- Employee_cart/Employee/src/componenets/Item.jsx
+++ Employee_cart/Employee/src/componenets/Item.jsx
@@ -1,11 +1,11 @@
-// import Data from './Data'
-// import Cart from './Cart'
+import Data from './Data'
+import Cart from './Cart'
 
-// export default function Item() {
-//   return (
-//     <div style={{display:"grid", gridTemplateColumns:'repeat(3,1fr)'}}>
-//       <Data />
-//       <Cart/>
-//     </div>
-//   )
-// }
+export default function Item() {
+  return (
+    <div style={{display:"grid", gridTemplateColumns:'repeat(3,1fr)'}}>
+      <Data />
+      <Cart/>
+    </div>
+  )
+}
