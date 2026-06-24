50d95895e1134c37da5a3d088d454dcb3540a660
Alpeshsorte <alpeshsorte4@gmail.com>
2026-06-24 17:59:10 +0530

    change

--- Day9_todolist/todolist/src/components/Todolist.jsx
+++ Day9_todolist/todolist/src/components/Todolist.jsx
@@ -1,34 +1,66 @@
 import React, { useState } from 'react'
 import Input from './Input'
 import './Todolist.css'
+import Edit from './Edit'
 export default function Todolist() {
 
   let [task, settask]=useState([])
+  let [editTask, setEditTask] = useState(null)
+
   function aaladata(taskobj){
 
     settask([...task, taskobj])
-    console.log(taskobj);
-    
-
+    // console.log(taskobj);
   }
+       // Delete Task
+    function deleteTask(id) {
+        let newTasks = tasks.filter(
+            item => item.id !== id
+        )
+
+        setTasks(newTasks)
+    }
+        // Open Edit Form
+    function editHandler(task) {
+        setEditTask(task)
+    }
+
+        // Update Task
+    function updateTask(updatedTask) {
+
+        let newTasks = tasks.map(item =>
+            item.id === updatedTask.id
+                ? updatedTask
+                : item
+        )
+
+        setTasks(newTasks)
+        setEditTask(null)
+    }
 
   return (
     <div  className="todo-container">
 
       <h1 className="todo-heading">Todo List</h1>
         <Input setdata={aaladata}/>
+            {
+                editTask &&
+                <Edit
+                    task={editTask}
+                    updateTask={updateTask}
+                />
+            }
         <div className="task-list">
-        {
-          task.map((i)=>(
-            <div className="task-card" key={i.id}>
-              <h3 className="task-name">{i.task}</h3>
-              <p className={`priority ${i.priority}`}>{i.priority}</p>
-                <button type="button">Edit</button>
-                <button type="button">Delete</button>
-            </div>
-          ))
-        }
-  
+            {
+              task.map((i)=>(
+                <div className="task-card" key={i.id}>
+                  <h3 className="task-name">{i.task}</h3>
+                  <p className={`priority ${i.priority}`}>{i.priority}</p>
+                    <button  onClick={() => editHandler(i)} type="button">Edit</button>
+                    <button   onClick={() => deleteTask(i.id)} type="button">Delete</button>
+                </div>
+              ))
+            }
         </div>
        
     </div>
