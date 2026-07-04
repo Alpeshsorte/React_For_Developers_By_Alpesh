0512c09e1db11621a2c7b98ef0b9b7a802747d1b
Alpeshsorte <alpeshsorte4@gmail.com>
2026-07-04 15:57:50 +0530

    sideeffect

--- Day13_useEffect_hooks/myapp/src/App.jsx
+++ Day13_useEffect_hooks/myapp/src/App.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+// import React from 'react'
+// import Effect from './components/Effect'
+// import Cart from './components/Cart'
+// export default function App() {
+//   return (
+//     <div>
+    
+//       <Cart/>
+//     </div>
+//   )
+// }
+
+
+
+
+
+
+import React, { useEffect, useState } from 'react'
 import Effect from './components/Effect'
 import Cart from './components/Cart'
+
 export default function App() {
+  
+  let [count,setCount]=useState(0)
+  let [timer,setTimer]=useState(true)
+  
+  useEffect(function(){
+
+    console.log("the ano function is been called")
+   console.log("...")
+   console.log("state of timer is been changed")
+  },[timer,count])
+  
+  useEffect(()=>{
+     getData()
+  },[])
+  
+  async function getData(){
+     
+    let data=await fetch("https://fakestoreapi.com/products")
+    let actualData=await data.json()
+    console.log(actualData)
+  }
+    
+ 
+  
   return (
     <div>
-    
-      <Cart/>
+          <h1>{count}</h1>
+          <button onClick={()=>{
+            setCount(count+1)
+          }}>inc</button>
+           
+           <button onClick={()=>{
+                
+                setTimer(timer?false:true)
+
+           }}>{timer ? "timer is true" : "timer is false"}</button>
+
+
+           <Cart/>
     </div>
   )
 }
